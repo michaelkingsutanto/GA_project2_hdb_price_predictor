0378d3414ede93dca0d8da89ed4ae9f4629fefe4
michaelks <michaelkingsutanto@gmail.com>
2024-05-20 00:43:47 +0800

    Add README.md and slidedeck

---
+++ streamlit/app.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+import pickle
+import pandas as pd
+
+# Load your model
+with open('hdb/lr_model.pkl', 'rb') as rf:
+    model = pickle.load(rf)
+
+# Title
+st.title("HDB Resale Price Prediction")
+
+# Create 2 columns
+col1, col2 = st.columns(2)
+
+# Col 1
+area = col1.selectbox('Planning Area', ['Ang Mo Kio', 'Bedok', 'Bishan', 'Bukit Batok', 'Bukit Merah',
+       'Bukit Panjang', 'Bukit Timah', 'Changi', 'Choa Chu Kang',
+       'Clementi', 'Downtown Core', 'Geylang', 'Hougang', 'Jurong East',
+       'Jurong West', 'Kallang', 'Marine Parade', 'Novena', 'Outram',
+       'Pasir Ris', 'Punggol', 'Queenstown', 'Rochor', 'Sembawang',
+       'Sengkang', 'Serangoon', 'Tampines', 'Tanglin', 'Toa Payoh',
+       'Western Water Catchment', 'Woodlands', 'Yishun'])
+flat_type = col1.selectbox('Flat Type', ['1 ROOM', '2 ROOM', '3 ROOM', '4 ROOM', '5 ROOM', 'EXECUTIVE', 'MULTI-GENERATION'])
+mid_storey = col1.number_input('Floor Number', value=5, min_value=1, max_value=50)
+distance_to_bus = col1.number_input('Distance to Bus Station (m)', value=500, step = 100, min_value=50, max_value=3000)
+distance_to_hawker = col1.number_input('Distance to Hawker Centre (m)', value=500, step = 100, min_value=50, max_value=3000)
+
+# Col2
+size = col2.number_input('Size (sqft)', value=500, step = 100, min_value=200, max_value=2650)
+age = col2.number_input('Age (year)', value=10, min_value=0, max_value=95)
+distance_to_mrt = col2.number_input('Distance to MRT Station (m)', value=500, step = 100, min_value=50, max_value=3000)
+distance_to_mall = col2.number_input('Distance to Mall (m)', value=500, step = 100, min_value=50, max_value=3000)
+distance_to_primary_school = col2.number_input('Distance to Primary School (m)', value=500, step = 100, min_value=50, max_value=3000)
+
+# Predict Button
+flat_type_ohe = {'1 ROOM': [0, 0, 0, 0, 0, 0],
+                 '2 ROOM': [1, 0, 0, 0, 0, 0],
+                 '3 ROOM': [0, 1, 0, 0, 0, 0],
+                 '4 ROOM': [0, 0, 1, 0, 0, 0],
+                 '5 ROOM': [0, 0, 0, 1, 0, 0],
+                 'EXECUTIVE': [0, 0, 0, 0, 1, 0],
+                 'MULTI-GENERATION': [0, 0, 0, 0, 0, 1]
+                }
+area_ohe = {'Ang Mo Kio':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Bedok':[1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Bishan':[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Bukit Batok':[0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Bukit Merah':[0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Bukit Panjang':[0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Bukit Timah':[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Changi':[0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Choa Chu Kang':[0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Clementi':[0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Downtown Core':[0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Geylang':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Hougang':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Jurong East':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Jurong West':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Kallang':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Marine Parade':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Novena':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Outram':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Pasir Ris':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Punggol':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Queenstown':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Rochor':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Sembawang':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            'Sengkang':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+            'Serangoon':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+            'Tampines':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+            'Tanglin':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            'Toa Payoh':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            'Western Water Catchment':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+            'Woodlands':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+            'Yishun':[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+           }
+hdb = [size, age, mid_storey, distance_to_mrt, distance_to_bus, distance_to_mall, distance_to_hawker, distance_to_primary_school] + flat_type_ohe[flat_type] + area_ohe[area]
+
+features = pd.DataFrame(hdb).transpose()
+
+if st.button('Predict'):
+    prediction = int(model.predict(features)[0])
+    formatted_prediction = "{:,.0f}".format(prediction)
+    # st.write(f'Predicted Price: ${formatted_prediction}')
+    st.write(f'Predicted Price: <span style="font-size:24px; color:green;">${formatted_prediction}</span>', unsafe_allow_html=True)
+
+
